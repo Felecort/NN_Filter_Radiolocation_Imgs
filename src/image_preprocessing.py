be79389824a7c9a2a56042315e3d4c4da54bcab6
FriLDD <dotpointen@gmail.com>
2022-10-07 18:56:36 +0300

    The file was divided into two parts

--- src/image_preprocessing.py
+++ src/image_preprocessing.py
@@ -1,7 +1,7 @@
 import numpy as np
 from random import shuffle
 from PIL import ImageOps, Image
-from os import listdir
+
 from operator import mul
 from functools import reduce
 
@@ -19,32 +19,6 @@ def get_total_length(img_path, img_list) -> list[list]:
         shuffle(arr)
     return idx_arrs
 ##################################################
-
-
-def assign_name_to_dataset(dataset_name, win_size) -> str:
-    """
-    Return the valid dataset_name
-    """
-    if dataset_name is None:
-        dataset_name = f"data_win{win_size}.csv"
-    else:
-        assert isinstance(dataset_name, str), "Dataset name shuold be str"
-        assert len(dataset_name) > 0, "Name shouldn't be empty line"
-        dataset_name = dataset_name.replace(" ", "_")
-        if dataset_name[-4:] != ".csv":
-            dataset_name += ".csv"
-    return dataset_name
-
-
-def check_existing_datasets(dataset_name, datasets_path) -> None:
-    """
-    Checks if the dataset exists.
-    If True new dataset won't be created
-    """
-    datasets = listdir(datasets_path)
-    assert dataset_name not in datasets,\
-        f"Dataset '{dataset_name}' already exists, change the window size"
-
 
 
 def add_noise(img, win_size, scale=0.2707)-> np.array:
