bb3a3d0737ab1f8c4f9ea434105f457d6e0c76cc
FriLDD <dotpointen@gmail.com>
2022-10-09 16:53:37 +0300

    added pre generator

--- src/dataset_creator.py
+++ src/dataset_creator.py
@@ -1,10 +1,13 @@
+from random import choice, randint
 import pandas as pd
 import numpy as np
 from os import listdir
 from PIL import Image, ImageOps
 from tqdm import tqdm
-from image_preprocessing import *
-from check_valid_data import *
+from image_processing import *
+from check_validity_of_values import *
+from pprint import pprint
+
 
 # Define global constants
 datasets_path = r"..\datasets\csv_files"
@@ -14,79 +17,108 @@ img_path = r"..\datasets\images"
 def generate_csv(*, win_size: int,
                  dump_to_file: int,
                  step:int = 1,
-                 img_path= img_path,
+                 img_path=img_path,
                  datasets_path=datasets_path,
                  dataset_name=None) -> None:
     """
     This function create dataset using certan
     window on images with borders.
     """
-
     #############################################################
     # Checking valid name, win_size and existing dataset
-    assert (win_size % 2) == 1, "The win_size should be odd"
-
+    assert ((win_size % 2) == 1) and (win_size > 0), "The win_size should be odd positive"
     dataset_name = assign_name_to_dataset(dataset_name, win_size)
-
     check_existing_datasets(dataset_name, datasets_path)
     #############################################################
 
     # Define constants
     counter = 0
-    win_size_square = win_size * win_size
-    data_arr = np.empty((dump_to_file, win_size_square + 1), dtype=float)
-    imgs_list = listdir(img_path)
-    half_win_size = win_size // 2
-
-    """ Test dataset shuffle """
-    # shuffled_idxs = get_total_length(img_path, imgs_list, step)
-    # print(shuffled_idxs[0][:20])
-    # print(shuffled_idxs[2][:20])
-    # return
-    """ Test dataset shuffle """
+    # win_size_square = win_size * win_size
+    # half_win_size = win_size // 2
+    # data_arr = np.empty((dump_to_file, win_size_square + 1), dtype=float)
+    list_of_img_names = listdir(img_path)
+    create_dataset = True
     
-    for file_name in tqdm(imgs_list):
+    load_images(img_path, list_of_img_names)
+    return
+    
+    shuffled_idxs, keys_list = get_shuffled_idxs(img_path=img_path, list_of_img_names=list_of_img_names, step=step)
+    
+    
+    while create_dataset:
+        # Random choose image, row(key) and val(column)
+        
+        # Get an index if random image 
+        chosen_img_idx = randint(0, len(shuffled_idxs) - 1)
+        
+        # Get image by a random index
+        chosen_img = shuffled_idxs[chosen_img_idx]
+        
+        # Get y indexes-column, the keys in a dict 
+        chosen_keys = keys_list[chosen_img_idx]
+        
+        # Get random key-row 
+        row = choice(chosen_keys)
+        
+        # Get row by random index(key) in a dict
+        values = chosen_img[row]
+        
+        # Get last value. Values have been just shuffled
+        column = values.pop()
+        
+        
+        
+        return
 
-        # Load and convert image
-        img = ImageOps.grayscale(Image.open(f"{img_path}\{file_name}"))
 
-        # Creatre border
-        img_with_borders = add_borders(img, half_win_size,
-                                       img.size[0], img.size[1])
 
-        # Normalization
-        norm_img_with_borders = np.array(img_with_borders) / 255
+if __name__ == "__main__":
+    generate_csv(win_size=7, dump_to_file=1000, step=100,
+                 img_path=r"D:\Projects\PythonProjects\NIR\datasets\images",
+                 datasets_path=r"D:\Projects\PythonProjects\NIR\datasets\csv_files")
+    
+    # for file_name in tqdm(list_of_img_names):
 
-        for y in range(half_win_size, half_win_size + img.size[1]):
-            for x in range(half_win_size, half_win_size + img.size[0]):
+    #     # Load and convert image
+    #     img = ImageOps.grayscale(Image.open(f"{img_path}\{file_name}"))
 
-                # Define margins
-                left = x - half_win_size
-                top = y - half_win_size
-                right = x + half_win_size + 1
-                bottom = y + half_win_size + 1
+    #     # Creatre border
+    #     img_with_borders = add_borders(img, half_win_size,
+    #                                    img.size[0], img.size[1])
 
-                # Get cropped image
-                cropped_img = norm_img_with_borders[top:bottom, left:right]
+    #     # Normalization
+    #     norm_img_with_borders = np.array(img_with_borders) / 255
 
-                target_pixel = cropped_img[half_win_size, half_win_size]
+    #     for y in range(half_win_size, half_win_size + img.size[1]):
+    #         for x in range(half_win_size, half_win_size + img.size[0]):
 
-                # Add noise and flatten
-                noised_img = add_noise(cropped_img, win_size).flatten()
+    #             # Define margins
+    #             left = x - half_win_size
+    #             top = y - half_win_size
+    #             right = x + half_win_size + 1
+    #             bottom = y + half_win_size + 1
 
-                data_arr[counter, :win_size_square] = noised_img
-                data_arr[counter, win_size_square] = target_pixel
+    #             # Get cropped image
+    #             cropped_img = norm_img_with_borders[top:bottom, left:right]
 
-                counter += 1
-                if (counter % dump_to_file) == 0:
-                    df = pd.DataFrame(data_arr)
+    #             target_pixel = cropped_img[half_win_size, half_win_size]
 
-                    # 0...n, target
-                    df.to_csv(f"{datasets_path}\{dataset_name}",
-                              sep=",", mode='a', index=False, header=False)
-                    counter = 0
+    #             # Add noise and flatten
+    #             noised_img = add_noise(cropped_img, win_size).flatten()
 
-    df = pd.DataFrame(data_arr[:counter])
-    # [0, ..., n, target]
-    df.to_csv(f"{datasets_path}\data_win{win_size}.csv",
-              sep=",", mode='a', index=False, header=False)
+    #             data_arr[counter, :win_size_square] = noised_img
+    #             data_arr[counter, win_size_square] = target_pixel
+
+    #             counter += 1
+    #             if (counter % dump_to_file) == 0:
+    #                 df = pd.DataFrame(data_arr)
+
+    #                 # 0...n, target
+    #                 df.to_csv(f"{datasets_path}\{dataset_name}",
+    #                           sep=",", mode='a', index=False, header=False)
+    #                 counter = 0
+
+    # df = pd.DataFrame(data_arr[:counter])
+    # # [0, ..., n, target]
+    # df.to_csv(f"{datasets_path}\data_win{win_size}.csv",
+    #           sep=",", mode='a', index=False, header=False)
