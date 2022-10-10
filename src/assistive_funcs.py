0a7b9d246de2468ceaa70bfb11b55e354f41a5dc
FriLDD <dotpointen@gmail.com>
2022-10-10 21:32:26 +0300

    round func removed

--- src/assistive_funcs.py
+++ src/assistive_funcs.py
@@ -4,5 +4,5 @@ from time import time, sleep
 def delta_time() -> 'function':
     start = time()
     def wrapper() -> float:
-        return round((time() - start), 2)
+        return time() - start
     return wrapper
