58e041ac98f9ddd96a60a094e10cc61f427ad299
FriLDD <dotpointen@gmail.com>
2022-10-10 22:45:34 +0300

    refactor

--- src/assistive_funcs.py
+++ src/assistive_funcs.py
@@ -1,8 +1,9 @@
-from time import time, sleep
+from time import time
 
 
 def delta_time() -> 'function':
     start = time()
+
     def wrapper() -> float:
         return time() - start
     return wrapper
