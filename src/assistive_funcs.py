e440fc7fdea2845b174198d3daf1d0a24fd13547
FriLDD <dotpointen@gmail.com>
2022-10-11 21:54:09 +0300

    added convert to gs func

--- src/assistive_funcs.py
+++ src/assistive_funcs.py
@@ -1,4 +1,7 @@
 from time import time
+from os import listdir
+from PIL import Image, ImageOps
+import pathlib
 
 
 def delta_time() -> 'function':
@@ -7,3 +10,13 @@ def delta_time() -> 'function':
     def wrapper() -> float:
         return time() - start
     return wrapper
+
+
+def convert_to_grayscale(path_to_images):
+    p = pathlib.PureWindowsPath(path_to_images)
+    images_list = listdir(p)
+    root_folder = p.parent
+    for image_name in images_list:
+        path = f"{path_to_images}\{image_name}"
+        img = ImageOps.grayscale(Image.open(path))
+        img.save(f"{root_folder}\gray_images\gs_{image_name}")
