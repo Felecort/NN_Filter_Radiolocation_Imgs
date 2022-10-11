a365ddea5e4671b46a77e54b1acd804798552f8e
FriLDD <dotpointen@gmail.com>
2022-10-11 23:56:42 +0300

    optimized teh function to filtering images

--- src/assistive_funcs.py
+++ src/assistive_funcs.py
@@ -2,7 +2,7 @@ from time import time
 from os import listdir
 from PIL import Image, ImageOps
 import numpy as np
-from torch import Tensor
+from torch import Tensor, no_grad
 import pathlib
 from tqdm import tqdm
 from image_processing import add_borders
@@ -26,21 +26,35 @@ def convert_to_grayscale(path_to_images):
         img.save(f"{root_folder}\gray_images\gs_{image_name}")
 
 
+def get_batch(y, img, left, win_size, device):
+    res = np.empty((304, 169))
+    if left:
+        for x in range(0, 304):
+            res[x] = img[y:win_size+y, x:x+win_size].flatten()
+    else:
+        for i, x in enumerate(range(304, 608)):
+            res[i] = img[y:win_size+y, x:x+win_size].flatten()
+    return Tensor(res).float().to(device=device)
+                    
+    
+
+
 def filtering_image(model, path_to_image, image_name, win_size, device):
+    out_path = fr"D:\Projects\PythonProjects\NIR\datasets\filtered_imgs\{image_name}"
     path = f"{path_to_image}\{image_name}"
     img = Image.open(path)
     shape = img.size
     out_arr = np.empty(shape)
     img = np.array(add_borders(img, win_size)) / 255
-    for y in tqdm(range(shape[1])):
-        for x in range(shape[0]):
-            croped_img = img[y:win_size+y, x:x+win_size]
-            data = Tensor([croped_img.flatten()]).float()
-            data = data.to(device=device)
-            res = model(data)
-            out_arr[y, x] = res
+    model.eval()
+    with no_grad():
+        for y in tqdm(range(shape[1])):
+            res = model(get_batch(y, img, 1, win_size, device)).cpu()
+            out_arr[y, :304] = np.squeeze(np.array(res))
+            res = model(get_batch(y, img, 0, win_size, device)).cpu()
+            out_arr[y, 304:] = np.squeeze(np.array(res))
     out_arr *= 255
     out = np.where(out_arr >= 255, 255, out_arr)
-    out_img = Image.fromarray(out)
-    out_path = fr"D:\Projects\PythonProjects\NIR\datasets\filtered_imgs\{image_name}"
-    out_img.save(out_path)
+    out = out.astype(np.uint8)
+    out = Image.fromarray(out)
+    out.save(out_path)
