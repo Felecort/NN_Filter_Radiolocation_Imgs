fac331ae9e5955b18182dc911284b391af132fa7
Felecort <dotpointen@gmail.com>
2023-04-30 13:11:21 +0300

    refactor

--- src/anis_diff/main.m
+++ src/anis_diff/main.m
@@ -1,19 +1,52 @@
-clear all; clc; close all;
+clc; close all;
 
-t = 13;
-k = 0.05;
-delta_t = 0.25;
+% Anis diff parametrs
+DELTA_T = 0.25;
+K = 0.05;
+T = 13;
 
-source_path = "./noised/";
-out_path = "./filtered/";
-files = dir("./noised/*.jpg");
+% Rayleigh distr parametr
+SCALE = 0.2707;
 
-for i = 1:length(files)
-    disp(i)
-    name = files(i).name;
-    img = mat2gray(imread(source_path + name));
+% Paths
+NOISED_PATH = "./noised/";
+FILTERED_PATH = "./filtered/";
+ORIGINAL_PATH = "./original/";
+GRAY_PATH = "./gray_images/";
 
-    filtered_image = AnisotropicDiffusionExp(img, t, k, delta_t);
-    %imshow(filtered_image);
-    imwrite(filtered_image, out_path + name);
+% list of images in folder
+FILES = dir("./original/*.jpg");
+
+ssim_sum = 0;
+
+for i = 1:length(FILES)
+    img_name = FILES(i).name;
+
+    % Open image and conver to grayscale
+    original_img = mat2gray(rgb2gray(imread(ORIGINAL_PATH + img_name)));
+    
+    % Add noise to image
+    noised_img = add_noise(original_img, SCALE);
+    
+    % Apply anis diff
+    filtered_img = AnisotropicDiffusionExp(noised_img, T, K, DELTA_T);
+     
+    %figure('Name','Original'), imshow(original_img);
+    %figure('Name','Filtered'), imshow(filtered_img);
+    %figure('Name','Noised'), imshow(noised_img);
+    
+    % Check SSIM
+    ssim_value = ssim(original_img, filtered_img);    
+    data_log = [img_name, "ssim: ", (ssim_value)];
+    ssim_sum = ssim_sum + ssim_value;
+    disp(data_log);
+    
+    % Save images
+    imwrite(noised_img, NOISED_PATH + img_name);
+    imwrite(filtered_img, FILTERED_PATH + img_name);
+    imwrite(original_img, GRAY_PATH + img_name);
 end
+
+% Average ssim
+ssim_avg = ["ssim avg: ", ssim_sum / length(FILES)];
+disp(ssim_avg)
